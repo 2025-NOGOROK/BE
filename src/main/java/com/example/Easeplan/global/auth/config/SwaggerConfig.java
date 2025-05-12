023bca0197be45a97a949fe8ec53cdd1e7e21e19
LHL <lin1137001@gmail.com>
2025-05-12 16:02:40 +0900

    ✨Feat:Swaggerconfig 생성

---
+++ src/main/java/com/example/Easeplan/global/auth/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.example.Easeplan.global.auth.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("My API")
+                        .description("API 설명")
+                        .version("v1.0"));
+    }
+}
