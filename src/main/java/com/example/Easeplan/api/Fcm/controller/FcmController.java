84d6a645041d754ed383113926027561441fe0e8
LHL <lin1137001@gmail.com>
2025-05-17 02:25:31 +0900

    ✨Feat: FCM 주석

--- src/main/java/com/example/Easeplan/api/Fcm/controller/FcmController.java
+++ src/main/java/com/example/Easeplan/api/Fcm/controller/FcmController.java
@@ -1,21 +1,21 @@
-package com.example.Easeplan.api.Fcm.controller;
-
-import com.example.Easeplan.api.Fcm.service.FcmService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.*;
-@Tag(name = "FCM", description = "FCM API")
-@RestController
-@RequestMapping("/api/fcm")
-public class FcmController {
-    private final FcmService fcmService;
-    public FcmController(FcmService fcmService) { this.fcmService = fcmService; }
-    @Operation(summary = "FCM", description = """ 
-            특정 시간에 알림을 보냅니다.""")
-    @PostMapping("/send")
-    public String send(@RequestParam String token,
-                       @RequestParam String title,
-                       @RequestParam String body) throws Exception {
-        return fcmService.sendMessage(token, title, body);
-    }
-}
+//package com.example.Easeplan.api.Fcm.controller;
+//
+//import com.example.Easeplan.api.Fcm.service.FcmService;
+//import io.swagger.v3.oas.annotations.Operation;
+//import io.swagger.v3.oas.annotations.tags.Tag;
+//import org.springframework.web.bind.annotation.*;
+//@Tag(name = "FCM", description = "FCM API")
+//@RestController
+//@RequestMapping("/api/fcm")
+//public class FcmController {
+//    private final FcmService fcmService;
+//    public FcmController(FcmService fcmService) { this.fcmService = fcmService; }
+//    @Operation(summary = "FCM", description = """
+//            특정 시간에 알림을 보냅니다.""")
+//    @PostMapping("/send")
+//    public String send(@RequestParam String token,
+//                       @RequestParam String title,
+//                       @RequestParam String body) throws Exception {
+//        return fcmService.sendMessage(token, title, body);
+//    }
+//}
