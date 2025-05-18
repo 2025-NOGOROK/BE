a374aa7ad3a028ee0c308eb7f6bd312eb8fb9e88
LHL <lin1137001@gmail.com>
2025-05-19 02:33:38 +0900

    ✨Feature: test upload

--- src/test/java/com/example/Easeplan/EaseplanApplicationTests.java
+++ src/test/java/com/example/Easeplan/EaseplanApplicationTests.java
@@ -2,12 +2,12 @@ package com.example.Easeplan;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 class EaseplanApplicationTests {
-
 	@Test
-	void contextLoads() {
-	}
-
+	void contextLoads() {}
 }
+
