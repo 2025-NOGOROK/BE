35e07ec7d0a832d907fb9210087665f42617e190
LHL <lin1137001@gmail.com>
2025-05-19 00:08:44 +0900

    ✨Feature: firebase 위치 yml로 명시

--- src/main/java/com/example/Easeplan/api/Fcm/config/FirebaseConfig.java
+++ src/main/java/com/example/Easeplan/api/Fcm/config/FirebaseConfig.java
@@ -3,6 +3,7 @@ package com.example.Easeplan.api.Fcm.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -15,9 +16,12 @@ import java.io.InputStream;
 @EnableScheduling
 @Configuration
 public class FirebaseConfig {
+
+    @Value("${firebase.keyPath}")
+    private String keyPath;
     @PostConstruct
     public void initialize() throws IOException {
-        InputStream serviceAccount = new ClassPathResource("firebase/serviceAccountKey.json").getInputStream();
+        InputStream serviceAccount = getClass().getClassLoader().getResourceAsStream(keyPath);
 
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
