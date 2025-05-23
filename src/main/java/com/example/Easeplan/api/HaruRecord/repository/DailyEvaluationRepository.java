859127458d17285e37e315eedebc9daafb85471f
LHL <lin1137001@gmail.com>
2025-05-23 11:10:05 +0900

    ✨Feature: findAllByUserAndDateBetween으로 주간 기록

--- src/main/java/com/example/Easeplan/api/HaruRecord/repository/DailyEvaluationRepository.java
+++ src/main/java/com/example/Easeplan/api/HaruRecord/repository/DailyEvaluationRepository.java
@@ -5,8 +5,12 @@ import com.example.Easeplan.global.auth.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface DailyEvaluationRepository extends JpaRepository<DailyEvaluation, Long> {
     Optional<DailyEvaluation> findByUserAndDate(User user, LocalDate date);
+
+    //월,일요일 해당하는 날짜
+    List<DailyEvaluation> findAllByUserAndDateBetween(User user, LocalDate startDate, LocalDate endDate);
 }
