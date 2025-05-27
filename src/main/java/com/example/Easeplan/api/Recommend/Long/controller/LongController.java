41923fd732b7f6d0f882c21e1c065df4241d0ad2
hyo-lin <lin1137001@gmail.com>
2025-05-27 16:50:03 +0900

    ✨Feature: 긴추천 오퍼레이션

--- src/main/java/com/example/Easeplan/api/Recommend/Long/controller/LongController.java
+++ src/main/java/com/example/Easeplan/api/Recommend/Long/controller/LongController.java
@@ -3,13 +3,14 @@ package com.example.Easeplan.api.Recommend.Long.controller;
 import com.example.Easeplan.api.Recommend.Long.dto.RecommendationOption;
 import com.example.Easeplan.api.Recommend.Long.service.LongService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
-
+@Tag(name = "긴 추천", description = "공연예술 긴 추천 API")
 @RestController
 @RequestMapping("/api/long-recommend")
 @RequiredArgsConstructor
