dc747cf1cfa1a8d5d980fe1603e2bc69396c2794
hyo-lin <lin1137001@gmail.com>
2025-05-27 16:45:44 +0900

    ✨Feature: 긴 추천 JSON 응답의 "최상위 루트" 구조를 매핑하기 위한 DTO

---
+++ src/main/java/com/example/Easeplan/api/Recommend/Long/dto/CulturalEventInfoRoot.java
@@ -0,0 +1,9 @@
+package com.example.Easeplan.api.Recommend.Long.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class CulturalEventInfoRoot {
+    private CulturalEventInfoResponse culturalEventInfo;
+}
