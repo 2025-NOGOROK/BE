bc20d543cf8ef717260f94631259abcf13eb4297
hyo-lin <lin1137001@gmail.com>
2025-05-27 16:48:05 +0900

    ✨Feature:  UserChoice 엔티티의 DB 조작을 위한           CRUD 메서드를 자동 제공하는 JPA 표준 인터페이스

---
+++ src/main/java/com/example/Easeplan/api/Recommend/Long/repository/UserChoiceRepository.java
@@ -0,0 +1,6 @@
+package com.example.Easeplan.api.Recommend.Long.repository;
+
+import com.example.Easeplan.api.Recommend.Long.dto.UserChoice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserChoiceRepository extends JpaRepository<UserChoice, Long> {}
