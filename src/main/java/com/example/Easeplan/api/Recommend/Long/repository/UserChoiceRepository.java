de04d70db33ad35accd7aa86d9a691132eb1ef93
hyo-lin <lin1137001@gmail.com>
2025-06-04 03:29:55 +0900

    ✨Feature: 어제 일정에서 최근 저장된 일정으로 변경

--- src/main/java/com/example/Easeplan/api/Recommend/Long/repository/UserChoiceRepository.java
+++ src/main/java/com/example/Easeplan/api/Recommend/Long/repository/UserChoiceRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface UserChoiceRepository extends JpaRepository<UserChoice, Long> {
     // 전날 type="event" 긴 추천만 조회
@@ -13,5 +14,8 @@ public interface UserChoiceRepository extends JpaRepository<UserChoice, Long> {
     List<UserChoice> findByUserAndTypeAndStartTimeBetween(
             User user, String type, String startTime, String endTime
     );
+
+    Optional<UserChoice> findTopByUserAndTypeOrderByStartTimeDesc(User user, String type);
+
 
 }
