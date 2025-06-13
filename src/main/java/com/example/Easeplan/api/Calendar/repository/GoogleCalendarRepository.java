db6c53197bfc09b5547b18d98e789cc9de68abab
hyo-lin <lin1137001@gmail.com>
2025-06-13 22:15:51 +0900

    ✨Feature: @Repository 추가

--- src/main/java/com/example/Easeplan/api/Calendar/repository/GoogleCalendarRepository.java
+++ src/main/java/com/example/Easeplan/api/Calendar/repository/GoogleCalendarRepository.java
@@ -2,9 +2,10 @@ package com.example.Easeplan.api.Calendar.repository;
 
 import com.example.Easeplan.api.Calendar.domain.GoogleCalendarInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface GoogleCalendarRepository extends JpaRepository<GoogleCalendarInfo, String> {
     List<GoogleCalendarInfo> findByUserEmail(String email);
 }
