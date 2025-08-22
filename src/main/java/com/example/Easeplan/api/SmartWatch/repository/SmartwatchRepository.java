2b2cde4fe203e02e3c6168ef5c3b3453ba244d05
hyo-lin <lin1137001@gmail.com>
2025-08-22 23:06:06 +0900

    refactor: 타임스탬프 구간으로 변경

--- src/main/java/com/example/Easeplan/api/SmartWatch/repository/SmartwatchRepository.java
+++ src/main/java/com/example/Easeplan/api/SmartWatch/repository/SmartwatchRepository.java
@@ -1,30 +1,16 @@
 package com.example.Easeplan.api.SmartWatch.repository;
 
 import com.example.Easeplan.api.SmartWatch.domain.HeartRate;
-import com.example.Easeplan.api.SmartWatch.dto.HeartRateRequest;
-import com.example.Easeplan.api.SmartWatch.service.HeartRateAnalyzer;
-import com.example.Easeplan.global.auth.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 import java.util.Optional;
 
 public interface SmartwatchRepository extends JpaRepository<HeartRate, Long> {
-    // ✅ measuredAt 제거 → startTime으로 정렬
-    Optional<HeartRate> findTopByUserEmailOrderByStartTimeDesc(String email);
 
-    //최근 시간
-    @Query("SELECT h FROM HeartRate h WHERE h.user = :user AND h.startTime >= :startTime AND h.startTime <= :endTime")
-    List<HeartRate> findByUserAndStartTimeBetween(
-            @Param("user") User user,
-            @Param("startTime") String startTime,
-            @Param("endTime") String endTime
-    );
-    // 해당 월에 데이터 존재 여부
-    boolean existsByUserEmailAndStartTimeStartingWith(String email, String monthPrefix);
+    Optional<HeartRate> findTopByUserEmailOrderByTimestampDesc(String email);
 
-    List<HeartRate> findByUserEmailAndStartTimeStartingWith(String email, String dayPrefix);
+    List<HeartRate> findByUserEmailAndTimestampBetween(String email, Long startInclusive, Long endInclusive);
 
-
+    boolean existsByUserEmailAndTimestampBetween(String email, Long startInclusive, Long endInclusive);
 }
