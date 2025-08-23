73e40c631b7b6f9bf800e4a54ab088ed1abaccdc
hyo-lin <lin1137001@gmail.com>
2025-08-23 10:48:40 +0900

    refactor: 세터 제거 및 Jackson 필드 접근으로 역직렬화

--- src/main/java/com/example/Easeplan/api/Recommend/Long/dto/CulturalEventInfoResponse.java
+++ src/main/java/com/example/Easeplan/api/Recommend/Long/dto/CulturalEventInfoResponse.java
@@ -1,14 +1,16 @@
 package com.example.Easeplan.api.Recommend.Long.dto;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
-@Getter @Setter
+@Getter
+@NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
-
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class CulturalEventInfoResponse {
-    //공공데이터(OpenAPI)에서 실제로 쓰는 응답 필드명
-    private List<Event> row;
+    private List<Event> row; // OpenAPI 실제 필드명
 }
--- src/main/java/com/example/Easeplan/api/Recommend/Long/dto/CulturalEventInfoRoot.java
+++ src/main/java/com/example/Easeplan/api/Recommend/Long/dto/CulturalEventInfoRoot.java
@@ -1,9 +1,14 @@
 package com.example.Easeplan.api.Recommend.Long.dto;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
-@Getter @Setter
+@Getter
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class CulturalEventInfoRoot {
     private CulturalEventInfoResponse culturalEventInfo;
 }
